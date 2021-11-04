5e0082411da42fed41c28c7bd2aec0c4a7f88ad9
刘晓彬 <563859671@qq.com>
2021-11-04 12:26:59 +0800

    完善注释

--- src/concerns/ModifyProperty.php
+++ src/concerns/ModifyProperty.php
@@ -2,10 +2,24 @@
 
 namespace BusyPHP\swoole\concerns;
 
+use ReflectionException;
 use ReflectionObject;
 
+/**
+ * 通过类反射修改
+ * @author busy^life <busy.life@qq.com>
+ * @copyright (c) 2015--2021 ShanXi Han Tuo Technology Co.,Ltd. All rights reserved.
+ * @version $Id: 2021/11/4 下午12:03 ModifyProperty.php $
+ */
 trait ModifyProperty
 {
+    /**
+     * 修改类保护属性
+     * @param object $object 类
+     * @param mixed  $value 值
+     * @param string $property 属性
+     * @throws ReflectionException
+     */
     protected function modifyProperty($object, $value, $property = 'app')
     {
         $reflectObject = new ReflectionObject($object);

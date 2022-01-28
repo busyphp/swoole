c94b51bd972fd1247f67ebfd240856e564781344
刘晓彬 <563859671@qq.com>
2022-01-28 20:53:25 +0800

    调整

--- src/Pool.php
+++ src/Pool.php
@@ -5,18 +5,27 @@ namespace BusyPHP\swoole;
 use Smf\ConnectionPool\ConnectionPool;
 use think\helper\Arr;
 
+/**
+ * 连接池类
+ * @author busy^life <busy.life@qq.com>
+ * @copyright (c) 2015--2022 ShanXi Han Tuo Technology Co.,Ltd. All rights reserved.
+ * @version $Id: 2022/1/28 8:46 PM Pool.php $
+ */
 class Pool
 {
+    /**
+     * @var ConnectionPool[]
+     */
     protected $pools = [];
     
     
     /**
-     * @param string         $name
-     * @param ConnectionPool $pool
-     *
+     * 添加一个连接池
+     * @param string         $name 连接池名称
+     * @param ConnectionPool $pool 连接池对象
      * @return Pool
      */
-    public function add(string $name, ConnectionPool $pool)
+    public function add(string $name, ConnectionPool $pool) : self
     {
         $pool->init();
         $this->pools[$name] = $pool;
@@ -26,31 +35,45 @@ class Pool
     
     
     /**
-     * @param string $name
-     *
+     * 获取特定的连接池
+     * @param string $name 连接池名称
      * @return ConnectionPool
      */
-    public function get(string $name)
+    public function get(string $name) : ?ConnectionPool
     {
         return $this->pools[$name] ?? null;
     }
     
     
-    public function close(string $key)
+    /**
+     * 关闭特定的连接池
+     * @param string $name 连接池名称
+     * @return bool
+     */
+    public function close(string $name) : bool
     {
-        return $this->pools[$key]->close();
+        $pool = $this->get($name);
+        if (!$pool) {
+            return false;
+        }
+        
+        return $pool->close();
     }
     
     
     /**
-     * @return array
+     * 获取所有连接池
+     * @return array<string,ConnectionPool>
      */
-    public function getAll()
+    public function getAll() : array
     {
         return $this->pools;
     }
     
     
+    /**
+     * 关闭所有池
+     */
     public function closeAll()
     {
         foreach ($this->pools as $pool) {
@@ -61,16 +84,20 @@ class Pool
     
     /**
      * @param string $key
-     *
      * @return ConnectionPool
      */
-    public function __get($key)
+    public function __get(string $key)
     {
         return $this->get($key);
     }
     
     
-    public static function pullPoolConfig(&$config)
+    /**
+     * 生成连接池配置
+     * @param array $config
+     * @return array
+     */
+    public static function pullPoolConfig(&$config) : array
     {
         return [
             'minActive'         => Arr::pull($config, 'min_active', 0),

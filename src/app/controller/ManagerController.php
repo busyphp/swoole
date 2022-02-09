aa3fcfdeff82c032b05663bcc780ee019a25bf14
刘晓彬 <563859671@qq.com>
2022-02-09 21:38:40 +0800

    移除queue表安装逻辑

--- src/app/controller/ManagerController.php
+++ src/app/controller/ManagerController.php
@@ -16,44 +16,6 @@ use think\Response;
 class ManagerController extends PluginManager
 {
     /**
-     * 创建表SQL
-     * @var string[]
-     */
-    private $createTableSql = [
-        'jobs' => "CREATE TABLE `#__table_prefix__#jobs` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `queue` VARCHAR(255) NOT NULL,
-    `payload` LONGTEXT NOT NULL,
-    `attempts` TINYINT(3) UNSIGNED NOT NULL,
-    `reserve_time` INT(11) UNSIGNED DEFAULT NULL,
-    `available_time` INT(11) UNSIGNED NOT NULL,
-    `create_time` INT(11) UNSIGNED NOT NULL,
-    PRIMARY KEY (`id`),
-    KEY `queue` (`queue`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Swoole队列表'",
-        
-        'failed_jobs' => "CREATE TABLE `#__table_prefix__#failed_jobs` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `connection` TEXT NOT NULL,
-    `queue` TEXT NOT NULL,
-    `payload` LONGTEXT NOT NULL,
-    `exception` LONGTEXT NOT NULL,
-    `fail_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Swoole队列失败表'",
-    ];
-    
-    /**
-     * 删除表SQL
-     * @var string[]
-     */
-    private $deleteTableSql = [
-        "DROP TABLE IF EXISTS `#__table_prefix__#jobs`",
-        "DROP TABLE IF EXISTS `#__table_prefix__#failed_jobs`",
-    ];
-    
-    
-    /**
      * 返回模板路径
      * @return string
      */
@@ -70,27 +32,7 @@ class ManagerController extends PluginManager
      */
     public function install() : Response
     {
-        $model = SystemPlugin::init();
-        $model->startTrans();
-        try {
-            foreach ($this->deleteTableSql as $item) {
-                $this->executeSQL($item);
-            }
-            
-            foreach ($this->createTableSql as $item) {
-                $this->executeSQL($item);
-            }
-            
-            $model->setInstall($this->info->package);
-            
-            $model->commit();
-        } catch (Exception $e) {
-            $model->rollback();
-            
-            throw $e;
-        }
-        
-        $this->updateCache();
+        SystemPlugin::init()->setInstall($this->info->package);
         $this->logInstall();
         
         return $this->success('安装成功');
@@ -104,23 +46,7 @@ class ManagerController extends PluginManager
      */
     public function uninstall() : Response
     {
-        $model = SystemPlugin::init();
-        $model->startTrans();
-        try {
-            foreach ($this->deleteTableSql as $item) {
-                $this->executeSQL($item);
-            }
-            
-            $model->setUninstall($this->info->package);
-            
-            $model->commit();
-        } catch (Exception $e) {
-            $model->rollback();
-            
-            throw $e;
-        }
-        
-        $this->updateCache();
+        SystemPlugin::init()->setUninstall($this->info->package);
         $this->logUninstall();
         
         return $this->success('卸载成功');
